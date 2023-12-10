23cfe7253fbacbf2aa1b568dd96e13a46b951ab3
dmitry416 <93573566+dmitry416@users.noreply.github.com>
2023-12-10 12:45:38 +1000

    sprites

--- levels/tilesets/floor.tsx
+++ levels/tilesets/floor.tsx
@@ -1,10 +1,25 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="floor" tilewidth="32" tileheight="32" tilecount="2" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="floor" tilewidth="32" tileheight="32" tilecount="7" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="32" height="32" source="../../graphics/sprites/floor/0_soil.png"/>
+  <image width="32" height="32" source="../../graphics/sprites/floor/mountain_floor/0_soil.png"/>
  </tile>
  <tile id="1">
-  <image width="32" height="32" source="../../graphics/sprites/floor/1_grass.png"/>
+  <image width="32" height="32" source="../../graphics/sprites/floor/mountain_floor/1_grass.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="../../graphics/sprites/floor/cave_floor/0_stonefloor.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="../../graphics/sprites/floor/cave_floor/1_stonefloor.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="../../graphics/sprites/floor/cave_floor/2_lava.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="../../graphics/sprites/floor/cats_floor/0_parquet.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="../../graphics/sprites/floor/cats_floor/1_wall.png"/>
  </tile>
 </tileset>
